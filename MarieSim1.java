6ffe116140d7d6cb23dd0f96a2614e15203e84fe
donfreiday <donfreiday@gmail.com>
2018-11-13 18:36:36 -0500

    Use setVisible() rather than deprecated show()

--- MarieSim1.java
+++ MarieSim1.java
@@ -11,6 +11,6 @@ import MarieSimulator.MarieSim;
 public class MarieSim1 {
   public static void main(String args[]) {
     MarieSim sim = new MarieSim();
-    sim.show();
+    sim.setVisible(true);
   }
 } // MarieSim1
